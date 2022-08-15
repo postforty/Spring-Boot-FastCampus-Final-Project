63644c6cc63ca3670b9af6b1ae5b0d4f199dd7e3
postforty <75767073+postforty@users.noreply.github.com>
2022-08-15 17:42:21 +0900

    [ADD] JPA, entity

---
+++ board/src/main/java/com/example/board/model/entity/Board.java
@@ -0,0 +1,33 @@
+package com.example.board.model.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Board {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long seq;
+
+	private String titile;
+	
+	private String writer;
+	
+	private String content;
+	
+	private LocalDateTime regDate;
+	
+	private Long cnt;
+}
---
+++ board/src/main/java/com/example/board/repository/Repository.java
@@ -0,0 +1,10 @@
+package com.example.board.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.board.model.entity.Board;
+
+public interface Repository extends CrudRepository<Board, Long> {
+	
+
+}
