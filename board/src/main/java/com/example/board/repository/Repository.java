e69102bc3e119780921476e38d2a15d738527747
postforty <75767073+postforty@users.noreply.github.com>
2022-08-15 18:20:15 +0900

    [UPDATE] UPDATE 테스트

--- board/src/main/java/com/example/board/repository/Repository.java
+++ board/src/main/java/com/example/board/repository/Repository.java
@@ -1,9 +1,12 @@
 package com.example.board.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.board.model.entity.Board;
 
 public interface Repository extends CrudRepository<Board, Long> {
 	
+	Optional<Board> findBySeq(Long seq);
 }
--- board/src/test/java/com/example/board/repository/BoardRepositoryTest.java
+++ board/src/test/java/com/example/board/repository/BoardRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.example.board.repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,18 +18,48 @@ public class BoardRepositoryTest {
 	@Autowired
 	private Repository repository;
 	
+//	@Test
+//	@Transactional
+//	public void create() {
+//		Board board = new Board();
+//		board.setTitle("제목2");
+//		board.setWriter("작성자2");
+//		board.setContent("내용2");
+//		board.setRegDate(LocalDateTime.now());
+//		board.setCnt(0L);
+//		
+//		Board newBoard = repository.save(board);
+//		System.out.println("newBoard : "+newBoard);
+//	}
+	
 	@Test
-	public void create() {
-		Board board = new Board();
-		board.setTitle("제목1");
-		board.setWriter("작성자1");
-		board.setContent("내용1");
-		board.setRegDate(LocalDateTime.now());
-		board.setCnt(0L);
+	@Transactional
+	public void read() {
 		
-		Board newBoard = repository.save(board);
-		System.out.println("newBoard : "+newBoard);
-				
+		// select * from user where id = ?
+//		Optional<Board> board = repository.findBySeq(1L);
+		Optional<Board> board = repository.findBySeq(1L);
+		
+		board.ifPresent(selectBoard ->{
+			System.out.println("board : "+selectBoard);
+			
+		});
 	}
+	
+//	@Test
+//	@Transactional
+//	public void update() {
+//		Optional<Board> board = repository.findBySeq(1L);
+//		
+//		board.ifPresent(selectBoard ->{
+//			selectBoard.setTitle("제목1_수정");
+//			selectBoard.setWriter("작성자1_수정");
+//			selectBoard.setContent("내용1_수정");
+//			selectBoard.setRegDate(LocalDateTime.now());
+//			selectBoard.setCnt(0L);
+//			
+//			repository.save(selectBoard);
+//		});
+//	}
 
 }
